ffd1ca258717ebb9a8230e87c8b6ee9271bef233
fkanjodo <fkanjodo@gmail.com>
2018-02-18 02:20:46 +0200

    The Signup From guys
    Hey team, this is the form that I came up with. You can make changes where need be. I included the html and the php  files of the same form for your easy adjustments

---
+++ SigningUp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sign Up</title>
+</head>
+
+<body>
+<body id="body-color"> <div id="Sign-Up"> <fieldset style="width:30%"><legend>Registration Form</legend> <table border="0"> <tr> <form method="POST" action="connectivity-sign-up.php"> <td>First Name</td><td> <input type="text" name="name"><td>Middle Name</td><td> <input type="text" name="name"><td>Last Name</td><td> <input type="text" name="name"></td> <tr><td>Program</td><td> <input type="text" name="name"></tr> <tr> <td>Email</td><td> <input type="text" name="email"></td> </tr> <tr> <td>UserName</td><td> <input type="text" name="user"></td> </tr> <tr> <td>Password</td><td> <input type="password" name="pass"></td> </tr> <tr> <td>Confirm Password </td><td><input type="password" name="cpass"></td> </tr> <tr> <td><input id="button" type="submit" name="submit" value="Submit"></td> </tr> </form> </table> </fieldset> </div>
+</body>
+</html>
