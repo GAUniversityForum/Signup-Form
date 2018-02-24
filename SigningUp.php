c0674e3e0cfc18a1097eb3f3ede8ce1f1502740a
fkanjodo <fkanjodo@gmail.com>
2018-02-24 14:08:41 +0200

    Registration Form
    Team, I have reworked on the Registration Form

--- SigningUp.php
+++ SigningUp.php
@@ -6,6 +6,10 @@
 </head>
 
 <body>
-<body id="body-color"> <div id="Sign-Up"> <fieldset style="width:30%"><legend>Registration Form</legend> <table border="0"> <tr> <form method="POST" action="connectivity-sign-up.php"> <td>First Name</td><td> <input type="text" name="name"><td>Middle Name</td><td> <input type="text" name="name"><td>Last Name</td><td> <input type="text" name="name"></td> <tr><td>Program</td><td> <input type="text" name="name"></tr> <tr> <td>Email</td><td> <input type="text" name="email"></td> </tr> <tr> <td>UserName</td><td> <input type="text" name="user"></td> </tr> <tr> <td>Password</td><td> <input type="password" name="pass"></td> </tr> <tr> <td>Confirm Password </td><td><input type="password" name="cpass"></td> </tr> <tr> <td><input id="button" type="submit" name="submit" value="Submit"></td> </tr> </form> </table> </fieldset> </div>
+<body id="body-color"> <div id="Sign-Up"> <fieldset style="width:30%"><legend>Registration Form</legend> <table border="0"> <tr> <form method="POST" action="connectivity-sign-up.php"> <td>First Name</td><td> <input type="text" name="name"><td>Middle Name</td><td> <input type="text" name="name"><td>Last Name</td><td> <input type="text" name="name"></td> <tr><td>Program</td><td><select name="Course">
+<option value="-1" selected></option>
+<option value="BSc BC">Bsc (Hons) Business Computing</option>
+<option value="BSs BIT">BSc (Hons) Business Information Technology</option>
+</select></td></tr> <tr> <td>Email</td><td> <input type="text" name="email"></td> </tr> <tr> <td>UserName</td><td> <input type="text" name="user"></td> </tr> <tr> <td>Password</td><td> <input type="password" name="pass"></td> </tr> <tr> <td>Confirm Password </td><td><input type="password" name="cpass"></td> </tr> <tr> <td><input id="button" type="submit" name="submit" value="Submit"></td> </tr> </form> </table> </fieldset> </div>
 </body>
 </html>
