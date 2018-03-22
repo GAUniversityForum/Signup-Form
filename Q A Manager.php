5826c9411162ba2cefc477c6ecbea833866d20c8
fkanjodo <fkanjodo@gmail.com>
2018-03-22 23:50:09 +0200

    The Q A Manager form guys
    Mebers, please find the Quality Manager form for your input.
    I am having  challenges working on the delete and edit buttons and I hope you will chip in and help

---
+++ Q A Manager.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>QA Manager</title>
+</head>
+
+<body>
+<div align="center">
+  <h1>Q A Manager
+  </h1>
+</div>
+<table width="450" border="0" cellpadding="0">
+  <tr>
+    <th scope="col">Home</th>
+    <th scope="col">Logout</th>
+    <th scope="col">View</th>
+  </tr>
+</table>
+<p>
+  <input type="submit" name="Enrol" id="Enrol" value="Enrol Student" />
+  <label for="textfield"></label>
+  <input type="text" width="300" name="textfield" id="textfield" />
+  <input type="submit" name="Search" id="Search" value="Search" />
+
+<p>&nbsp;	<div class="pagination">
+  <a href="#">&laquo;</a>
+  <a href="#">1-</a>
+  <a class="active" href="#">-5</a>
+    <a href="#">&raquo;</a>
+</div> </p>
+
+<table width="550" border="0" cellpadding="0">
+  <tr>
+    <th width="20" scope="col">&nbsp;</th>
+    <th width="55" scope="col">Student ID</th>
+    <th width="83" scope="col">Username</th>
+    <th width="86" scope="col">Password</th>
+    <th width="82" scope="col">Email</th>
+    <th width="117" scope="col">Department</th>
+    <th width="37" scope="col">&nbsp;</th>
+    <th width="34" scope="col">&nbsp;</th>
+  </tr>
+  <tr>
+    <th scope="row"><form id="form1" name="form1" method="post" action="">
+      <input type="checkbox" name="checkbox" id="checkbox" />
+      <label for="checkbox"></label>
+    </form></th>
+    <td><form id="form4" name="form4" method="post" action="">
+      <label for="textfield2"></label>
+      <input type="text" name="student ID" id="ID" />
+    </form></td>
+    <td><form id="form5" name="form5" method="post" action="">
+      <label for="textfield3"></label>
+      <input type="text" name="username" id="username" />
+    </form></td>
+    <td><form id="form6" name="form6" method="post" action="">
+      <label for="textfield4"></label>
+      <input type="text" name="password" id="password" />
+    </form></td>
+    <td><form id="form7" name="form7" method="post" action="">
+      <label for="textfield5"></label>
+      <input type="text" name="email address" id="email address" />
+    </form></td>
+    <td><form id="form8" name="form8" method="post" action="">
+      <label for="textfield6"></label>
+      <input type="text" name="department" id="department" />
+    </form></td>
+    <td><form id="form2" name="form2" method="post" action="">
+      <input type="submit" name="edit button" id="button" value="Edit" />
+    </form></td>
+    <td><form id="form3" name="form3" method="post" action="">
+      <input type="submit" name="delete" id="delete" value="Del" />
+    </form></td>
+  </tr>
+  <tr>
+    <th scope="row"><input type="checkbox" name="checkbox2" id="checkbox2" /></th>
+    <td><input type="text" name="student ID" id="ID" /></td>
+    <td><input type="text" name="username" id="username" /></td>
+    <td><input type="text" name="password" id="password" /></td>
+    <td><input type="text" name="email address" id="email address" /></td>
+    <td><input type="text" name="department" id="department" /></td>
+    <td><input type="submit" name="button3" id="button3" value="Edit" /></td>
+    <td><input type="submit" name="button7" id="button7" value="Del" /></td>
+  </tr>
+  <tr>
+    <th scope="row"><input type="checkbox" name="checkbox3" id="checkbox3" /></th>
+    <td><input type="text" name="student ID" id="ID" /></td>
+    <td><input type="text" name="username" id="username" /></td>
+    <td><input type="text" name="password" id="password" /></td>
+    <td><input type="text" name="email address" id="email address" /></td>
+    <td><input type="text" name="department" id="department" /></td>
+    <td><input type="submit" name="button4" id="button4" value="Edit" /></td>
+    <td><input type="submit" name="button8" id="button8" value="Del" /></td>
+  </tr>
+  <tr>
+    <th scope="row"><input type="checkbox" name="checkbox4" id="checkbox4" /></th>
+    <td><input type="text" name="student ID" id="ID" /></td>
+    <td><input type="text" name="username" id="username" /></td>
+    <td><input type="text" name="password" id="password" /></td>
+    <td><input type="text" name="email address" id="email address" /></td>
+    <td><input type="text" name="department" id="department" /></td>
+    <td><input type="submit" name="button5" id="button5" value="Edit" /></td>
+    <td><input type="submit" name="button9" id="button9" value="Del" /></td>
+  </tr>
+  <tr>
+    <th scope="row"><input type="checkbox" name="checkbox5" id="checkbox5" /></th>
+    <td><input type="text" name="student ID" id="ID" /></td>
+    <td><input type="text" name="username" id="username" /></td>
+    <td><input type="text" name="password" id="password" /></td>
+    <td><input type="text" name="email address" id="email address" /></td>
+    <td><input type="text" name="department" id="department" /></td>
+    <td><input type="submit" name="button6" id="button6" value="Edit" /></td>
+    <td><input type="submit" name="button10" id="button10" value="Del" /></td>
+  </tr>
+</table>
+</body>
+</html>
